a2f0a46e587023091f2eb6b5947f8f8e0b1b08ea
Vladimir <prosto24061989@gmail.com>
2019-07-09 15:49:49 +0300

    install gulp

---
+++ gulp/gulpfile.js
@@ -0,0 +1,41 @@
+var gulp           = require('gulp');
+var less           = require('gulp-less');
+var autoprefixer   = require('gulp-autoprefixer');
+var browserSync    = require('browser-sync');
+var fileinclude    = require('gulp-file-include')
+
+function watch(){
+    browserSync.init({
+        server: {
+            baseDir: './dist/'
+        },
+    });
+
+    gulp.watch('app/less/**/*.less', styles)
+    gulp.watch('app/html/**/*.html', html)
+}
+
+function styles() {
+    return gulp.src('app/less/**/*.less')
+        .pipe(less())
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.stream())
+}
+
+function html(){
+    return gulp.src('app/html/index.html')
+        .pipe(fileinclude({
+            prefix: '@@'
+        }))
+        .pipe(gulp.dest('dist/'))
+        .pipe(browserSync.stream())
+}
+
+gulp.task('styles', styles);
+gulp.task('watch', watch);
+gulp.task('html', html);
+
+
+
+
